bb4ade4788794c8453b3de664ffd35612c114a5d
fordpass <fordpass@MGC000002276.itbic.ford.com>
2019-09-24 14:13:47 -0400

    Multiple tabs

---
+++ MultiTab/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MultiTab
+//
+//  Created by fordpass on 9/24/19.
+//  Copyright © 2019 fordpass. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MultiTab/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MultiTab
+//
+//  Created by fordpass on 9/24/19.
+//  Copyright © 2019 fordpass. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
